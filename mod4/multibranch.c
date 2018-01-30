eef49560becb4335e1af20c238cfd0d3a20cbe9a
Kevin Hoffman <kevinhoffman@mail.weber.edu>
2018-01-30 10:14:47 -0700

    Initial commit

---
+++ mod4/multibranch.c
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  multibranch.c
+ *
+ *    Description:  Nested branches
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2018 10:10:25 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Kevin Hoffman (), kevinhoffman@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int income = 30000;
+    if(income)       // IF income !=0
+    {
+        printf("Income is %d\n", income);
+    }
+    
+    else
+    { 
+        printf("No income\n");
+    }
+
+    return 0;
+}
+// Function Definitions
+
+
