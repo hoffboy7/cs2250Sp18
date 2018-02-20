8fd716b58d591039f562e9f3f9818e2ec383252e
Kevin Hoffman <kevinhoffman@mail.weber.edu>
2018-02-20 09:28:45 -0700

    Commit

---
+++ mod6/hwMod6pt2.c
@@ -0,0 +1,63 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hwMod6pt2.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/15/2018 09:39:21 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Kevin Hoffman (), kevinhoffman@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+const int PLAYERS = 5;
+const int ATTRI = 5;
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    //1) Define arrays for values
+    int ar[PLAYERS];
+
+    int ar[ATTRI]; 
+
+    //1.1) Fill in values
+    
+    //2) Display menu
+MENU
+    printf("u - Update player rating")
+    char 
+    a - Output players above a rating
+    r - Replace player
+    o - Output roster
+    q - Quit
+    //3) Implement the "Output roster" menu option
+    
+    //4) Implement the "Update player rating" menu option. 
+    //Prompt the user for a player's jersey number. 
+    //Prompt again for a new rating for the player, and then change that player's rating. (1 pt) 
+    
+     //5) Implement the "Output players above a rating" menu option. 
+     //Prompt the user for a rating. 
+     //Print the jersey number and rating for all players with ratings above the entered value. (2 pts) 
+     
+     //6)Implement the "Replace player" menu option. Prompt the user for the jersey number of the player to replace. 
+     //If the player is in the roster, then prompt again for a new jersey number and rating. 
+     //Update the replaced player's jersey number and rating. (2 pts) 
+
+    Choose an option:
+    return 0;
+}
+// Function Definitions
+
+
